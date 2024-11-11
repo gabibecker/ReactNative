0218a1bedc2278c647ae1d1514b15ae00e2b13bd
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-11 15:01:11 -0300

    Organizando Componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import Primeiro from './components/Primeiro';
 
-export default ()  => <Text>Primeiro Componente!!!</Text>;
-
+export default () => (
+    <SafeAreaView>
+        <Primeiro />
+    </SafeAreaView>
+);
