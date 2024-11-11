73cb7f2108012ca096c8bc16141df6933e15a0a2
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-11 14:46:13 -0300

    Primeiro Componente #02

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-function App() {
-    return <Text>Primeiro Componente</Text>;
-}
+export default ()  => <Text>Primeiro Componente!!!</Text>;
 
-export default App;
