4d341de5050884772defacda04b0f83bac6ababc
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-11 15:32:14 -0300

    Sistema de Módulos do ECMAScript

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import Primeiro from './components/Primeiro';
 
+import X, { Comp1, Comp2 } from './components/Multi';
 export default () => (
     <SafeAreaView>
         <Primeiro />
+        <Comp1 />
+        <X />
+        <Comp2 />
     </SafeAreaView>
 );
