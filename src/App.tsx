a3f15a9234853098311a4a0a0e1903e6dfe9734f
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-13 17:19:51 -0300

    Usando botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
+import Botão from './components/Botão';
 // import Aleatorio from './components/Aleatorio';
 // import MinMax from './components/MinMax';
 // import Primeiro from './components/Primeiro';
 // import X, { Comp1, Comp2 } from './components/Multi';
-import Titulo from './components/Titulo';
+// import Titulo from './components/Titulo';
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <Titulo principal="cadastro" secundario="Tela de cadastro"/>
+        <Botão />
+        {/* <Titulo principal="cadastro" secundario="Tela de cadastro"/> */}
         {/* <Aleatorio min ={10} max={60}/>
         <Aleatorio min ={10} max={60}/>
         <Aleatorio min ={10} max={60}/>
