1aa27638dfe63eabbaf8cf4a9e6beb06e9b02941
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-13 17:46:20 -0300

    usando UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
-import Botão from './components/Botão';
+// import Botão from './components/Botão';
 // import Aleatorio from './components/Aleatorio';
 // import MinMax from './components/MinMax';
 // import Primeiro from './components/Primeiro';
 // import X, { Comp1, Comp2 } from './components/Multi';
 // import Titulo from './components/Titulo';
+import Contador from './components/Contador';
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <Botão />
+        <Contador inicial={100} passo={2} />
+        <Contador/>
+        {/* <Botão /> */}
         {/* <Titulo principal="cadastro" secundario="Tela de cadastro"/> */}
         {/* <Aleatorio min ={10} max={60}/>
         <Aleatorio min ={10} max={60}/>
