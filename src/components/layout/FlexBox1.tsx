48806048fae819f5ee76ce3ef2d2e7241c129148
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-18 17:00:01 -0300

    Usando flexBox #02

--- src/components/layout/FlexBox1.tsx
+++ src/components/layout/FlexBox1.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
-import estilo from "../estilo";
-import Quadrado from "./Quadrado";
+import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import estilo from '../estilo';
+import Quadrado from './Quadrado';
 
 export default props => {
     return (
-        <SafeAreaView style={style.FlexV1}>
+        <SafeAreaView style={style.FlexV2}>
             <Quadrado color="#ff801a" />
             <Quadrado color="#50d1f6" />
             <Quadrado color="#dd22c1" />
@@ -16,9 +16,11 @@ export default props => {
 };
 
 const style = StyleSheet.create({
-    FlexV1: {
-        backgroundColor: "#000",
-        flexGrow: 1,
-        justifyContent: "flex-start",
+    FlexV2: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'space-evenly',
+        alignItems:'flex-end',
+        backgroundColor: '#000',
     },
 });
