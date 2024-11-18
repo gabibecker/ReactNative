6a5e2cc80d000c4d9a406532f7f58a0e5a08207b
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-18 17:59:25 -0300

    Utilizando o this

--- src/components/mega/Mega.tsx
+++ src/components/mega/Mega.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from "react";
 import { Text, TextInput } from "react-native";
 import estilo from "../estilo";
@@ -8,20 +9,21 @@ export default class Mega extends Component {
         qtdNumeros: this.props.qtdNumeros
     };
 
-    alterarQtdNumero(qtde) {
-        this.setState({qtdeNumeros: qtde})
-    }
+    alterarQtdNumero = (qtde) => {
+        this.setState({ qtdNumeros: qtde }); // Corrige o nome do estado
+    };
 
     render () {
-        
         return (
             <>
-            <Text style = {estilo.fontG}> Gerador de Mega-Sena {this.state.qtdNumeros}</Text>
-            <TextInput 
-            placeholder="Qtde de Números"
-            value = {this.state.qtdNumeros}
-            onChange={this.alterarQtdNumero}
-            />
+                <Text style={estilo.fontG}> Gerador de Mega-Sena {this.state.qtdNumeros}</Text>
+                <TextInput
+                    keyboardType={'numeric'}
+                    style={{ borderBottomWidth: 1 }}
+                    placeholder="Qtde de Números"
+                    value={String(this.state.qtdNumeros)} // Certifique-se de que o valor seja uma string
+                    onChangeText={this.alterarQtdNumero} // Use onChangeText
+                />
             </>
         );
     }
