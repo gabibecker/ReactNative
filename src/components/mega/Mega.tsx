66ffb65d46ac970dbcc759cfe37ebdd15fb81b84
Gabriela Cristina Becker <g.becker@unochapeco.edu.br>
2024-11-19 13:54:43 -0300

    Componente Mega-Sena #01

--- src/components/mega/Mega.tsx
+++ src/components/mega/Mega.tsx
@@ -1,29 +1,48 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from "react";
-import { Text, TextInput } from "react-native";
+import { Button, Text, TextInput } from "react-native";
 import estilo from "../estilo";
 
 export default class Mega extends Component {
 
     state = {
-        qtdNumeros: this.props.qtdNumeros
+        qtdNumeros: this.props.qtdNumeros,
+        numeros: [],
     };
 
     alterarQtdNumero = (qtde) => {
-        this.setState({ qtdNumeros: qtde }); // Corrige o nome do estado
+        this.setState({ qtdNumeros: +qtde });
+    };
+
+    gerarNumeroNaoContido = nums => {
+        const novo = parseInt(Math.random() * 60 ) + 1;
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo;
+    };
+
+    gerarNumeros = () => {
+        const numeros = Array(this.state.qtdNumeros) 
+            .fill(null) 
+            .reduce((n, _) => [...n, this.gerarNumeroNaoContido(n)], [])
+            .sort((a,b) => a-b );
+            this.setState({ numeros });
     };
 
     render () {
         return (
             <>
-                <Text style={estilo.fontG}> Gerador de Mega-Sena {this.state.qtdNumeros}</Text>
+                <Text style={estilo.fontG}> Gerador de Mega-Sena </Text>
                 <TextInput
                     keyboardType={'numeric'}
                     style={{ borderBottomWidth: 1 }}
                     placeholder="Qtde de Números"
-                    value={String(this.state.qtdNumeros)} // Certifique-se de que o valor seja uma string
-                    onChangeText={this.alterarQtdNumero} // Use onChangeText
+                    value={`${this.state.qtdNumeros}`}
+                    onChangeText={this.alterarQtdNumero}
                 />
+                <Button
+                    title= 'Gerar'
+                    onPress={this.gerarNumeros}
+                />
+                <Text>{this.state.numeros.join(',')}</Text>
             </>
         );
     }
